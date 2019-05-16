537e169c96baa990bd438d00adc4f1ac00989858
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-17 00:02:31 +0300

    Added tests for Aged Brie and Sulfuras

--- php/test/GildedRoseTest.php
+++ php/test/GildedRoseTest.php
@@ -14,7 +14,9 @@ class GildedRoseTest extends TestCase {
         $this->assertEquals("foo", $items[0]->name);
     }
 
-    /*Normal product*/
+    /*
+     * Normal product
+     */
     public function testNormalProductUpdate()
     {
         $item = [new Item('Elixir of the Mongoose', 10, 10)];
@@ -59,4 +61,75 @@ class GildedRoseTest extends TestCase {
         $this->assertEquals($item[0]->sell_in, -6);
         $this->assertEquals($item[0]->quality, 0);
     }
+
+    /*
+     * Aged Brie
+     */
+    public function testAgedBrieUpdate()
+    {
+        $item = [new Item('Aged Brie', 10, 10)];
+        $gildedRose = new GildedRose($item);
+        $gildedRose->update_quality();
+        $this->assertEquals($item[0]->sell_in, 9);
+        $this->assertEquals($item[0]->quality, 11);
+    }
+
+    public function testAgedBrieUpdateWhenSellInNegative()
+    {
+        $item = [new Item('Aged Brie', -5, 10)];
+        $gildedRose = new GildedRose($item);
+        $gildedRose->update_quality();
+        $this->assertEquals($item[0]->sell_in, -6);
+        $this->assertEquals($item[0]->quality, 12);
+    }
+
+    public function testAgedBrieUpdateWhenSellInEnds()
+    {
+        $item = [new Item('Aged Brie', 0, 10)];
+        $gildedRose = new GildedRose($item);
+        $gildedRose->update_quality();
+        $this->assertEquals($item[0]->sell_in, -1);
+        $this->assertEquals($item[0]->quality, 12);
+    }
+
+    public function testAgedBrieUpdateWhenQualityMaximum()
+    {
+        $item = [new Item('Aged Brie', 10, 50)];
+        $gildedRose = new GildedRose($item);
+        $gildedRose->update_quality();
+        $this->assertEquals($item[0]->sell_in, 9);
+        $this->assertEquals($item[0]->quality, 50);
+    }
+
+    public function testAgedBrieUpdateWhenQualityMaximumAndSellInNegative()
+    {
+        $item = [new Item('Aged Brie', -5, 50)];
+        $gildedRose = new GildedRose($item);
+        $gildedRose->update_quality();
+        $this->assertEquals($item[0]->sell_in, -6);
+        $this->assertEquals($item[0]->quality, 50);
+    }
+
+    /*
+     * Sulfuras, Hand of Ragnaros
+     */
+    public function testSulfurasUpdate()
+    {
+        $item = [new Item('Sulfuras, Hand of Ragnaros', 10, 80)];
+        $gildedRose = new GildedRose($item);
+        $gildedRose->update_quality();
+        $this->assertEquals($item[0]->sell_in, 10);
+        $this->assertEquals($item[0]->quality, 80);
+    }
+
+    public function testSulfurasUpdateWhenSellInNegative()
+    {
+        $item = [new Item('Sulfuras, Hand of Ragnaros', -6, 80)];
+        $gildedRose = new GildedRose($item);
+        $gildedRose->update_quality();
+        $this->assertEquals($item[0]->sell_in, -6);
+        $this->assertEquals($item[0]->quality, 80);
+    }
+
+
 }

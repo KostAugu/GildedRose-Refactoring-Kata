f5394c4b525d7304d60472439b838dafbf64fda4
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-17 20:53:25 +0300

    Updated Conjured class to decrease quality twice as fast

--- php/src/Model/Product/Conjured.php
+++ php/src/Model/Product/Conjured.php
@@ -6,5 +6,19 @@ namespace App\Model\Product;
 
 class Conjured extends Product
 {
-
+    /**
+     * @return int
+     */
+    public function getQualityChange(): int
+    {
+        $qualityChange = 0;
+        if ($this->qualityMoreThan(0)) {
+            if ($this->qualityMoreThan(3) && $this->sellInIsLessThan(1)) {
+                $qualityChange = -4;
+            } else {
+                $qualityChange = -2;
+            }
+        }
+        return $qualityChange;
+    }
 }

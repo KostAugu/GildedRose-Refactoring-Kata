215cf540bc5515be01cfab7e6e807ffa0cfe8e0b
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-17 16:44:21 +0300

    Created new class ProductsFactory

---
+++ php/src/Model/ProductsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Model;
+
+
+use App\Model\Product\AgedBrie;
+use App\Model\Product\BackstagePass;
+use App\Model\Product\StandardProduct;
+use App\Model\Product\Sulfuras;
+
+class ProductsFactory
+{
+    public static function create(Item $item)
+    {
+        switch ($item->name) {
+            case 'Aged Brie':
+                return new AgedBrie($item);
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                return new BackstagePass($item);
+            case 'Sulfuras, Hand of Ragnaros':
+                return new Sulfuras($item);
+            default:
+                return new StandardProduct($item);
+        }
+    }
+}

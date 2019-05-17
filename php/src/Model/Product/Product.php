87e9b04bb2bb434e51f9191c9228f1f2bc2caf7e
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-17 18:56:03 +0300

    Updated Product class with methods for standard product

--- php/src/Model/Product/Product.php
+++ php/src/Model/Product/Product.php
@@ -8,6 +8,9 @@ use App\Model\Item;
 
 class Product
 {
+    /*
+     * @var Item
+     */
     protected $item;
 
     public function __construct(Item $item)
@@ -16,4 +19,54 @@ class Product
 
     }
 
+    public function changeQuality()
+    {
+        $number = $this->getQualityChange();
+        $this->item->quality = $this->item->quality + $number;
+    }
+
+    public function changeSellIn()
+    {
+        $this->item->sell_in = $this->item->sell_in - 1;
+    }
+
+    /**
+     * @return bool
+     */
+    public function sellInIsNegative(): bool
+    {
+        return $this->item->sell_in < 0;
+    }
+
+    /**
+     * @return bool
+     */
+    public function qualityMoreThanZero(): bool
+    {
+        return $this->item->quality > 0;
+    }
+
+    /**
+     * @return bool
+     */
+    public function qualityMoreThanOne(): bool
+    {
+        return $this->item->quality > 1;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQualityChange(): int
+    {
+        $qualityChange = 0;
+        if ($this->qualityMoreThanZero()) {
+            if ($this->qualityMoreThanOne() && $this->sellInIsNegative()) {
+                $qualityChange = -2;
+            } else {
+                $qualityChange = -1;
+            }
+        }
+        return $qualityChange;
+    }
 }

ec3afc6a5cf804c8fb356ac82b7f7ee1c418bcaf
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-17 20:42:38 +0300

    Updated BackstagePass to increase and decrease quality

--- php/src/Model/Product/BackstagePass.php
+++ php/src/Model/Product/BackstagePass.php
@@ -6,5 +6,24 @@ namespace App\Model\Product;
 
 class BackstagePass extends Product
 {
+    /**
+     * @return int
+     */
+    public function getQualityChange(): int
+    {
+        $qualityChange = 0;
+        if ($this->qualityLessThan(50) && $this->sellInIsMoreThan(0)) {
+            if($this->sellInIsMoreThan(10)) {
+                $qualityChange = 1;
+            } elseif ($this->sellInIsLessThan(6)) {
+                $qualityChange = 3;
+            } else {
+                $qualityChange = 2;
+            }
 
+        } elseif ($this->sellInIsLessThan(1)) {
+            $qualityChange = -($this->item->quality);
+        }
+        return $qualityChange;
+    }
 }

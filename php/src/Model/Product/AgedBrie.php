2a69aa531f632abc9f66c635cc856f4fb65b9eb2
KosAugu <kostas.augustinavicius@yahoo.com>
2019-05-17 19:43:12 +0300

    Updated AgedBrie class to increase quality

--- php/src/Model/Product/AgedBrie.php
+++ php/src/Model/Product/AgedBrie.php
@@ -6,5 +6,20 @@ namespace App\Model\Product;
 
 class AgedBrie extends Product
 {
+    /**
+     * @return int
+     */
+    public function getQualityChange(): int
+    {
+        $qualityChange = 0;
+        if ($this->qualityLessThan(50)) {
+            if ($this->qualityLessThan(49) && $this->sellInIsNegative()) {
+                $qualityChange = 2;
+            } else {
+                $qualityChange = 1;
+            }
+        }
+        return $qualityChange;
+    }
 
 }
